f1f2c83b27db0f7ff67ffa9c55223e83540dcc7b
Leo Ochoa <lochoa@redhat.com>
2023-02-21 17:21:00 +0100

    precache: Refactor olm pre-caching to reduce extra cycles

--- pre-cache/parse_index.py
+++ pre-cache/parse_index.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import traceback
 
+
 def parse_args():
     """ Parse the command line arguments """
     parser = argparse.ArgumentParser(
@@ -19,16 +20,16 @@ def parse_args():
                         type=argparse.FileType('a'),
                         help="Path to the image list file (appended).")
 
-    args = parser.parse_args()
+    args_ = parser.parse_args()
     if len(sys.argv) < 3:
         parser.print_help()
         exit(1)
-    return args
+    return args_
 
 
-def extract_images(args, objects):
+def extract_images(args_):
     """ Extract related images from the rendered index
-    
+
     Input parameters:
     args: command line arguments, contain operators_spec_file.name
     objects: a list of parsed index objects that comply to OLM schema
@@ -43,13 +44,13 @@ def extract_images(args, objects):
 
     Returns: list of image pull specifications
     """
-    bundles, packages, images = [], [], []
+    bundles, packages, images_ = [], [], []
     channels = {}
     # 1. Form the operators packages and channels structure
-    with open(args.operators_spec_file.name, 'r') as p:
+    with open(args_.operators_spec_file.name, 'r') as p:
         # "records" is a list of list: [package, channel] items
         records = [i.split(":") for i in p.read().splitlines() if len(i) > 0]
-    
+
     for item in records:
         if len(item) != 2:
             print(f"operators record {item} is malformed, skipping...")
@@ -58,39 +59,41 @@ def extract_images(args, objects):
         channels[package] = channel
         packages.append(package)
         print(f"will process package {package} channel {channel}")
+
     # 2. Find the right channels for our packages and get the latest bundle
+    objects = load_rendered_index(packages)
     for item in objects:
-        if item.get("schema") == "olm.channel":
-            if item.get("package") in packages and item.get("name") == channels[item.get("package")]:
-                latest = item.get("entries")[-1].get("name")
-                bundles.append(latest)
+        if item.get("name") == channels[item.get("package")]:
+            latest = item.get("entries")[-1].get("name")
+            bundles.append(latest)
+
     # 3. extract related images from our bundles
     for item in objects:
-        if item.get("schema") == "olm.bundle":
-            if item.get("name") in bundles and item.get("package") in packages:
-                images.extend([elem.get("image") for elem in item.get("relatedImages")])
-    return images
+        if item.get("name") in bundles:
+            images_.extend([elem.get("image") for elem in item.get("relatedImages")])
+    return images_
 
 
-def load_rendered_index():
-    with open(args.rendered_index.name, args.rendered_index.mode) as f:
-        data = f.read().lstrip()
+def load_rendered_index(packages):
+    with open(args.rendered_index.name, args.rendered_index.mode) as file:
+        data_ = file.read().lstrip()
     # Rendered index is not a valid json, but a list
-    # of concatenated json blocks. Hence the raw decoder and the loop
+    # of concatenated json blocks. Hence, the raw decoder and the loop
     decoder = json.JSONDecoder()
     objects = []
-    while data:
-        obj, index = decoder.raw_decode(data)
-        objects.append(obj)
-        data = data[index:].lstrip()
+    while data_:
+        obj, index = decoder.raw_decode(data_)
+        if obj["schema"] == "olm.channel" or obj["schema"] == "olm.bundle":
+            if obj["package"] in packages:
+                objects.append(obj)
+        data_ = data_[index:].lstrip()
     return objects
 
 
 if __name__ == "__main__":
     try:
         args = parse_args()
-        data = load_rendered_index()
-        images = extract_images(args, data)
+        images = extract_images(args)
         with open(args.img_list_file.name, args.img_list_file.mode) as f:
             f.write('\n'.join(images))
             f.write('\n')
